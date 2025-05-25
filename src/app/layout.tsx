009a33666276eef59be64fdd0804ff32cd91f25d
mrtdurdenthe2 <kai.wil.business@gmail.com>
2025-05-25 17:03:00 +0100

    added posthog for tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/NavBar";
+import { PostHogProvider } from "@/components/PostHogProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,12 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black flex flex-col items-center min-h-screen`}
       >
-        <div className="w-full flex-1 flex flex-col items-center justify-center pb-16">
-          {children}
-        </div>
-        <div className="fixed bottom-14 left-0 w-full flex justify-center px-14">
-          <NavBar />
-        </div>
+        <PostHogProvider>
+          <div className="w-full flex-1 flex flex-col items-center justify-center pb-16">
+            {children}
+          </div>
+          <div className="fixed bottom-14 left-0 w-full flex justify-center px-14">
+            <NavBar />
+          </div>
+        </PostHogProvider>
       </body>
     </html>
   );
